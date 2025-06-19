22809372cd71574f92ef72e1cd02f607b8bfe59d
zhangyu2333 <zy19990223zy@163.com>
2025-06-19 13:27:03 +0800

    fix:修改tailwind版本

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home: React.FC = () => {
   return (
-    <div style={{ padding: '20px', backgroundColor: '#f0f2f5' }}>
+    <div className="p-20 bg-gray-50">
       <h1>Welcome to the Home Page</h1>
       <p>This is a simple home page component.</p>
       <p>You can add more content here as needed.</p>
